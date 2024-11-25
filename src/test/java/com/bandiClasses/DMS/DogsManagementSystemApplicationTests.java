a7f70ca6891ad4555dd583b68f482ccbbcfb383f
Sai Achyuth Konda <saiachyuthkonda@gmail.com>
2024-11-25 00:24:58 -0600

    modified

--- src/test/java/com/bandiClasses/DMS/DogsManagementSystemApplicationTests.java
+++ src/test/java/com/bandiClasses/DMS/DogsManagementSystemApplicationTests.java
@@ -1,13 +1,9 @@
-package com.greeshmaClasses.DMS;
+package com.bandiClasses.DMS;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * 
- * 
- * @author Greeshma Borra - S567537
- */
+
 @SpringBootTest
 class DogsManagementSystemApplicationTests {
 
